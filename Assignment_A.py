094ee1f4433f761f93f6271ddeedeed0ec47d159
Radar3759 <chainmailgirl@gmail.com>
2024-09-23 17:55:05 -0700

    first commit

---
+++ Assignment_A.py
@@ -0,0 +1,29 @@
+#takes a name as input
+name = input("\nWhat is your name? " );
+#prints name
+print("Hi, " + name + "\n")
+#takes a breakfast as input
+breakfast = input("What is your favorite breakfast? ")
+#prints name and type of breakfast
+print(name + ", your favorite breakfast is: " + breakfast + "\n")
+
+# declare vars x, y, z, zString
+x = 3;
+y = 5;
+z = x * y;
+# makes result of z a string
+zString = str(z);
+# if else statements
+if x > y:
+    print("x is greater than y\n")
+else:
+    print("y is greater than x\n")    
+# if else statements
+if (x + y) > 5:
+    print("Greater than 5!\n")
+else: 
+    print("Less than 5!")
+
+print (x * y)
+print("\nThe product of x and y is " + zString +" \n")
+print(x - y)
